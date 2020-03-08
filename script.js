86e93faa23ca00f359a8466a597483f9cd416e35
Albert Goncalves <goncalves.albert@gmail.com>
2020-03-08 17:50:16 -0400

    onload

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-WebAssembly.instantiateStreaming(fetch("./bin/main.wasm"))
-    .then(function(object) {
-        var buffer = object.instance.exports.memory.buffer;
-        var string =
-            new TextDecoder("utf8").decode(new Uint8Array(buffer, 0, 13));
-        console.log(string);
-        console.log(new Uint32Array(buffer.slice(13, 17)));
-        object.instance.exports.mutate();
-        console.log(new Uint32Array(buffer.slice(13, 17)));
-    });
+window.onload = function() {
+    WebAssembly.instantiateStreaming(fetch("./bin/main.wasm"))
+        .then(function(object) {
+            var buffer = object.instance.exports.memory.buffer;
+            var string =
+                new TextDecoder("utf8").decode(new Uint8Array(buffer, 0, 13));
+            console.log(string);
+            console.log(new Uint32Array(buffer.slice(13, 17)));
+            object.instance.exports.mutate();
+            console.log(new Uint32Array(buffer.slice(13, 17)));
+        });
+}
