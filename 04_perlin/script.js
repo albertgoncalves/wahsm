ab08b53021b9885cae7277ee40e0922f5d38fd8a
Albert Goncalves <goncalves.albert@gmail.com>
2020-03-22 17:32:48 -0400

    sketch out pixel memory

--- 04_perlin/script.js
+++ 04_perlin/script.js
@@ -5,18 +5,24 @@ window.onload = function() {
             var byteOffset = {
                 gradients: 0,
                 permutations: 128 << 2,
+                pixels: (128 << 2) * 3,
             };
             var typedLength = {
                 gradients: 128 << 1,
                 permutations: 128 << 1,
+                pixels: 128 << 9,
             };
             var gradients = new Int16Array(memory, byteOffset.gradients,
                                            typedLength.gradient);
             var permutations = new Float32Array(
                 memory, byteOffset.permutations, typedLength.permutations);
+            var pixels = new Uint8Array(memory, byteOffset.pixels,
+                                        typedLength.pixels)
             object.instance.exports.main();
             console.log(gradients.slice(126 << 1, 128 << 1));
             console.log(permutations.slice(0, 16));
             console.log(permutations.slice(126 << 1, 128 << 1));
+            console.log(pixels.slice((2 << 9) - 4, 2 << 9))
+            console.log(pixels.slice((128 << 9) - 4, 128 << 9))
         });
 };
